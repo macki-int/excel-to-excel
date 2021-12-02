3355274d0ff3a50ee203d226e3f58be6b107e519
Marek <macki@int.pl>
2021-12-02 21:47:47 +0100

    rename app excel-to-excel

--- src/main/java/pl/trollsysstems/exceltoexcel/ExcelToExcelController.java
+++ src/main/java/pl/trollsysstems/exceltoexcel/ExcelToExcelController.java
@@ -3,7 +3,7 @@ package pl.trollsysstems.exceltoexcel;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class ExcelToExcelController {
     @FXML
     private Label welcomeText;
 
