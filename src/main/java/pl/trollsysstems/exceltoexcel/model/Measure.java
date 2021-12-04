5edee819e68024dbaf576814750edcf2d595fb20
Marek <macki@int.pl>
2021-12-04 23:04:24 +0100

    add id field in Measure class

--- src/main/java/pl/trollsysstems/exceltoexcel/model/Measure.java
+++ src/main/java/pl/trollsysstems/exceltoexcel/model/Measure.java
@@ -1,4 +1,51 @@
 package pl.trollsysstems.exceltoexcel.model;
 
+import java.time.LocalDateTime;
+
 public class Measure {
+    private int id;
+    private LocalDateTime measureDateAndTime;
+    private Double angelX;
+    private Double angleY;
+    private Double temperature;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getMeasureDateAndTime() {
+        return measureDateAndTime;
+    }
+
+    public void setMeasureDateAndTime(LocalDateTime measureDateAndTime) {
+        this.measureDateAndTime = measureDateAndTime;
+    }
+
+    public Double getAngelX() {
+        return angelX;
+    }
+
+    public void setAngelX(Double angelX) {
+        this.angelX = angelX;
+    }
+
+    public Double getAngleY() {
+        return angleY;
+    }
+
+    public void setAngleY(Double angleY) {
+        this.angleY = angleY;
+    }
+
+    public Double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Double temperature) {
+        this.temperature = temperature;
+    }
 }
