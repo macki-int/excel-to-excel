d074a8839daab6b95bc1522beeb4f7a26b681f57
Marek <macki@int.pl>
2021-12-05 14:37:23 +0100

    add open file dialog

--- src/main/java/pl/trollsysstems/exceltoexcel/ExcelToExcelController.java
+++ src/main/java/pl/trollsysstems/exceltoexcel/ExcelToExcelController.java
@@ -1,14 +1,47 @@
 package pl.trollsysstems.exceltoexcel;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
 
 public class ExcelToExcelController {
+//    @FXML
+//    private Button buttonExit;
+//    @FXML
+//    private Button buttonOpenConfigDialog;
     @FXML
-    private Label welcomeText;
+    private Label labelPath;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private void onButtonExitClick() {
+        Platform.exit();
+        System.exit(0);
+    }
+
+    @FXML
+    private void onButtonOpenConfigDialogClick() {
+        Stage stage = new Stage();
+        FileChooser fileChooser = new FileChooser();
+
+        fileChooser.setTitle("Otwórz plik konfiguracyjny");
+        configureFileChooser(fileChooser);
+        File file = fileChooser.showOpenDialog(stage);
+        if (file != null) {
+            labelPath.setWrapText(true);
+            labelPath.setText(file.toString());
+        }
+    }
+
+    private void configureFileChooser(FileChooser fileChooser) {
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Wszystkie pliki", "*.*"),
+                new FileChooser.ExtensionFilter("XLSX", "*.xlsx"),
+                new FileChooser.ExtensionFilter("XLS", "*.xls")
+        );
     }
 }
+
