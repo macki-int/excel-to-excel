9ddeab3be9a7230dc2b00ab3b1957f43e71ed0a1
Marek <macki@int.pl>
2021-12-05 14:35:42 +0100

    add app icon

--- src/main/java/pl/trollsysstems/exceltoexcel/ExcelToExcel.java
+++ src/main/java/pl/trollsysstems/exceltoexcel/ExcelToExcel.java
@@ -3,6 +3,7 @@ package pl.trollsysstems.exceltoexcel;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,11 @@ public class ExcelToExcel extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(ExcelToExcel.class.getResource("excel-to-excel-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 640, 480);
+        Scene scene = new Scene(fxmlLoader.load(), 575, 500);
+
+        Image image = new Image("file:src/main/resources/excel_icon.png");
+
+        stage.getIcons().add(image);
         stage.setTitle("ExcelToExcel");
         stage.setScene(scene);
         stage.show();
