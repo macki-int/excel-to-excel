4e1ce2d0e4eccdaf8114791cec2d8ac2d7e42be3
macki-int <macki@int.pl>
2021-12-20 00:05:25 +0100

    add read data from excel file and fill table view

--- src/main/java/pl/trollsysstems/exceltoexcel/controller/ExcelToExcelController.java
+++ src/main/java/pl/trollsysstems/exceltoexcel/controller/ExcelToExcelController.java
@@ -1,16 +1,29 @@
 package pl.trollsysstems.exceltoexcel.controller;
 
 import javafx.application.Platform;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import pl.trollsysstems.exceltoexcel.model.InclinometerImportParam;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.*;
 
 public class ExcelToExcelController {
     //    @FXML
@@ -19,6 +32,16 @@ public class ExcelToExcelController {
 //    private Button buttonOpenConfigDialog;
     @FXML
     private Label labelPath;
+    @FXML
+    private TableView<InclinometerImportParam> tableViewConfig;
+    @FXML
+    private TableColumn<InclinometerImportParam, String> columnNameConfig;
+    @FXML
+    private TableColumn<InclinometerImportParam, Integer> columnQuantityConfig;
+    @FXML
+    private TableColumn<InclinometerImportParam, LocalDate> columnStartDateConfig;
+    @FXML
+    private TableColumn<InclinometerImportParam, LocalDate> columnStopDateConfig;
 
     @FXML
     private void onButtonExitClick() {
@@ -38,7 +61,16 @@ public class ExcelToExcelController {
             labelPath.setWrapText(true);
             labelPath.setText(file.toString());
 
-            readConfigureFile(file);
+            List<InclinometerImportParam> inclinometerImportParamList = readConfigureFile(file);
+
+            ObservableList<InclinometerImportParam> observableArrayList = FXCollections
+                    .observableArrayList(inclinometerImportParamList);
+
+            tableViewConfig.setItems(observableArrayList);
+            columnNameConfig.setCellValueFactory(new PropertyValueFactory<>("inclinometerName"));
+            columnQuantityConfig.setCellValueFactory(new PropertyValueFactory<>("quantityInclinometerInChain"));
+            columnStartDateConfig.setCellValueFactory(new PropertyValueFactory<>("startDate"));
+            columnStopDateConfig.setCellValueFactory(new PropertyValueFactory<>("stopDate"));
         }
     }
 
@@ -50,10 +82,30 @@ public class ExcelToExcelController {
         );
     }
 
-    private void readConfigureFile(File file) throws IOException {
+    private List<InclinometerImportParam> readConfigureFile(File file) throws IOException {
         FileInputStream fis = new FileInputStream(file);
-        XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fis);
+        Workbook workbook = new XSSFWorkbook(fis);
 
+        Sheet sheet = workbook.getSheetAt(0);
+
+        List<InclinometerImportParam> inclinometerImportParamList = new ArrayList<>();
+        for (Row row : sheet) {
+            inclinometerImportParamList.add(getValueFromCells(row));
+        }
+        workbook.close();
+        return inclinometerImportParamList;
     }
+
+    private InclinometerImportParam getValueFromCells(Row row) {
+        InclinometerImportParam inclinometerImportParam = new InclinometerImportParam();
+
+        inclinometerImportParam.setInclinometerName(row.getCell(0).getStringCellValue());
+        inclinometerImportParam.setQuantityInclinometerInChain((int) row.getCell(1).getNumericCellValue());
+        inclinometerImportParam.setStartDate(row.getCell(2).getLocalDateTimeCellValue().toLocalDate());
+        inclinometerImportParam.setStopDate(row.getCell(4).getLocalDateTimeCellValue().toLocalDate());
+
+        return inclinometerImportParam;
+    }
+
 }
 
