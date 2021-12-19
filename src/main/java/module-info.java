78f1b5a807405077f55231a2c594b349de4e951e
macki-int <macki@int.pl>
2021-12-19 23:59:56 +0100

    add opens for model package

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,10 +4,12 @@ module pl.trollsysstems.exceltoexcel {
 
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
-    requires poi.ooxml;
+//    requires org.apache.poi.poi;
+    requires org.apache.poi.ooxml;
 
     opens pl.trollsysstems.exceltoexcel to javafx.fxml;
     exports pl.trollsysstems.exceltoexcel;
     exports pl.trollsysstems.exceltoexcel.controller;
     opens pl.trollsysstems.exceltoexcel.controller to javafx.fxml;
+    opens pl.trollsysstems.exceltoexcel.model to javafx.base;
 }
